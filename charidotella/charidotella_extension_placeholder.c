681f5ed1742b51c775ce2ac5df7370623d54bf50
Alexandre Marcireau <alexandre.marcireau@gmail.com>
2023-03-30 20:11:45 +1100

    Use NULL instead of nullptr

--- charidotella/charidotella_extension_placeholder.c
+++ charidotella/charidotella_extension_placeholder.c
@@ -1,6 +1,6 @@
 #include <Python.h>
 
-static PyMethodDef charidotella_extension_placeholder_methods[] = {{nullptr, nullptr, 0, nullptr}};
+static PyMethodDef charidotella_extension_placeholder_methods[] = {{NULL, NULL, 0, NULL}};
 static struct PyModuleDef charidotella_extension_placeholder_definition = {
     PyModuleDef_HEAD_INIT,
     "charidotella_extension_placeholder",
